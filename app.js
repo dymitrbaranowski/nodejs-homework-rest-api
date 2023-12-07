30e64efcafc7b19b5c7d35f91a12e9c0682f96ac
Dymitr <dmitribaranowski51@gmail.com>
2023-12-07 15:03:51 +0200

    fox2

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-import express from "express";
-import logger from "morgan";
-import cors from "cors";
-import "dotenv/config";
+import express from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+import 'dotenv/config';
 
-import authRouter from "./routes/api/auth-router.js";
+import authRouter from './routes/api/auth-router.js';
 
-import contactsRouter from "./routes/api/contacts-router.js";
+import contactsRouter from './routes/api/contacts-router.js';
 
 // dotenv.config();
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use("/api/auth", authRouter);
-app.use("/api/contacts", contactsRouter);
+app.use('/users', authRouter);
+app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
+  const { status = 500, message = 'Server error' } = err;
   res.status(status).json({
     message,
   });
--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -1,30 +1,40 @@
-import express from "express";
+import express from 'express';
 
-import authController from "../../controllers/auth-controller.js";
+import authController from '../../controllers/auth-controller.js';
 
-import { authenticate, isEmptyBody } from "../../middlewares/index.js";
+import { authenticate, isEmptyBody } from '../../middlewares/index.js';
 
-import { validateBody } from "../../decorators/index.js";
+import { validateBody } from '../../decorators/index.js';
 
-import { userSignupSchema, userSigninSchema } from "../../models/User.js";
+import { upload } from '../../middlewares/upload.js';
+
+import { userSignupSchema, userSigninSchema } from '../../models/User.js';
 
 const authRouter = express.Router();
 
 authRouter.post(
-  "/signup",
+  '/signup',
   isEmptyBody,
   validateBody(userSignupSchema),
   authController.signup
 );
 
 authRouter.post(
-  "/signin",
+  '/signin',
   isEmptyBody,
   validateBody(userSigninSchema),
   authController.signin
 );
 
-authRouter.get("/current", authenticate, authController.getCurrent);
+authRouter.get('/current', authenticate, authController.getCurrent);
 
-authRouter.post("/signout", authenticate, authController.signout);
+authRouter.post('/signout', authenticate, authController.signout);
+
+router.patch(
+  '/avatars',
+  authenticate,
+  upload.single('avatar'),
+  ctrl.updateAvatar
+);
+
 export default authRouter;
