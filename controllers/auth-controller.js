f5dde573656db57a86fee551c5a12dff1c03376d
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2023-12-14 21:37:09 +0200

    fox4

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -44,7 +44,7 @@ const signup = async (req, res) => {
   const verifyEmail = {
     to: email,
     subject: 'Verify email',
-    html: `<a target="_blank" href="${BASE_URL}/auth/verify/${verificationCode}">Click verify email</a>`,
+    html: `<a target="_blank" href="${BASE_URL}/users/verify/${verificationCode}">Click verify email</a>`,
   };
   await sendEmail(verifyEmail);
 
